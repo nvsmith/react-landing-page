1007e5ca7b986a04e3c6c1cd5bf0b0bea16e3300
Nathan Smith <nathanielvs@gmail.com>
2024-02-29 20:01:04 -0700

    Fix CallToAction child components.

--- components/villages/neighborhoods/blocks/CallToAction.js
+++ components/villages/neighborhoods/blocks/CallToAction.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Container, Row, Col } from "react-grid-system";
-
 import Title from "./houses/Title";
 import Subtitle from "./houses/Subtitle";
 import Button from "./houses/Button";
--- components/villages/neighborhoods/blocks/houses/Button.js
+++ components/villages/neighborhoods/blocks/houses/Button.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Container, Row, Col } from "react-grid-system";
 
 class Button extends React.Component {
     render() {
