09cbaf5faeaa66039023311390d292db92af42f3
Nathan Smith <nathanielvs@gmail.com>
2024-03-02 22:07:06 -0700

    Install React Router. Set up Router paths in index.js.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Container, Row, Col } from "react-grid-system";
 // import { createRoot } from "react-dom/client";
 import LandingPage from "./components/villages/LandingPage";
 import Demo from "./components/villages/Demo";
@@ -8,10 +10,12 @@ import css from "./styles/landing-page.css";
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <LandingPage />
-                <Demo />
-            </div>
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<LandingPage />} />
+                    <Route path="/demo" element={<Demo />} />
+                </Routes>
+            </Router>
         );
     }
 }
